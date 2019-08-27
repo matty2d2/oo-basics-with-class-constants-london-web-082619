b1245c841361fb0f35fbfa9b8091395d54206119
Matthew Dias Prada <matthew.dias.prada@gmail.com>
2019-08-27 22:05:03 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -4,7 +4,7 @@ class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
   
-  BRANDS = Set.new
+  BRANDS = Set.new                #sets are always unique
   
   def initialize(brand)
     @brand = brand
