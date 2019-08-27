f55ee1e20a7ac74bf6e4e99c5d82b61ba200a5cd
Matthew Dias Prada <matthew.dias.prada@gmail.com>
2019-08-27 22:08:57 +0000

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,9 +1,14 @@
+require 'set'
+
 class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
-
+  
+  BRANDS = Set.new                #sets always contain unique elements
+  
   def initialize(brand)
     @brand = brand
+    BRANDS << @brand #we can update in step-one since each shoe instance has a brand
   end
 
   def cobble
